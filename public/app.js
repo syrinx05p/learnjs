95b95b7e7f25e1b9a75934e9060bdd04ee4a0fc1
syrinx05p <syrinx05p@gmail.com>
2017-09-05 11:30:55 +0900

    p42

--- public/app.js
+++ public/app.js
@@ -2,15 +2,16 @@
 var learnjs = {};
 
 learnjs.problemView = function() {
-return $('<div class="problem-view">').text('Coming soon!');
+  return $('<div class="problem-view">').text('Coming soon!');
 }
 
 learnjs.showView = function(hash) {
   var routes = {
-    '#problem-1': learnjs.problemView
+    '#problem': learnjs.problemView
   };
-  var viewFn = routes[hash];
+  var hashParts = hash.split('-');
+  var viewFn = routes[hashParts[0]];
   if (viewFn) {
-    $('.view-container').empty().append(viewFn());
+    $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
