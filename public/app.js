220b3c20883b47f61c0c508243360e112ac3e72e
syrinx05p <syrinx05p@gmail.com>
2017-10-01 15:10:27 +0900

    201710011510

--- public/app.js
+++ public/app.js
@@ -1,25 +1,71 @@
 'use strict';
 var learnjs = {};
 
-learnjs.problemView = function (problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!';
-  return $('<div class="problem-view">').text(title);
+/* events */
+learnjs.appOnReady = function () {
+  window.onhashchange = function () {
+    learnjs.showView(window.location.hash);
+  };
+  learnjs.showView(window.location.hash);
 }
 
+learnjs.applyObject = function (obj, elem) {
+  for (var key in obj) {
+    elem.find('[data-name="' + key + '"]').text(obj[key]);
+  }
+};
+
+/* view */
 learnjs.showView = function (hash) {
+  // map hash and function
   var routes = {
     '#problem': learnjs.problemView
   };
+
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
+
   if (viewFn) {
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
 
-learnjs.appOnReady = function() {
-  window.onhashchange = function() {
-    learnjs.showView(window.location.hash);
-  };
-  learnjs.showView(window.location.hash);
+learnjs.problemView = function (data) {
+  var problemNumber = parseInt(data, 10);
+  var view = $('.templates .problem-view').clone();
+  var problemData = learnjs.problems[problemNumber - 1];
+  var resultFlash = view.find('.result');
+
+  function checkAnswer() {
+    var answer = view.find('.answer').val();
+    var test = problemData.code.replace('__', answer) + '; problem();';
+    //var test = problemData.code.replace('__', answer);
+    return eval(test);
+  }
+  function checkAnswerClick() {
+    if (checkAnswer()) {
+      resultFlash.text('Correct!');
+    } else {
+      resultFlash.text('Incorrect!');
+    }
+    return false;
+  }
+  view.find('.check-btn').click(checkAnswerClick);
+  view.find('.title').text('Problem #' + problemNumber);
+  learnjs.applyObject(problemData, view);
+  return view;
 }
+
+/* data */
+//source[learnjs/3100/public/app.js]{
+learnjs.problems = [
+  {
+    description: "What is truth?",
+    code: "function problem() { return __; }"
+  },
+  {
+    description: "Simple Math",
+    code: "function problem() { return 42 === 6 * __; }"
+  }
+];
+//}
