125952158ee017c5f3bcfb72d977df5978248c8a
syrinx05p <syrinx05p@gmail.com>
2017-10-01 18:38:36 +0900

    3.3.2

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
 'use strict';
 var learnjs = {};
 
+learnjs.template = function (name) {
+  return $(' .templates .' + name).clone();
+}
+
 /* events */
 learnjs.appOnReady = function () {
   window.onhashchange = function () {
@@ -32,28 +36,55 @@ learnjs.showView = function (hash) {
 
 learnjs.problemView = function (data) {
   var problemNumber = parseInt(data, 10);
-  var view = $('.templates .problem-view').clone();
+  var view = learnjs.template('problem-view');
   var problemData = learnjs.problems[problemNumber - 1];
   var resultFlash = view.find('.result');
 
   function checkAnswer() {
     var answer = view.find('.answer').val();
     var test = problemData.code.replace('__', answer) + '; problem();';
-    //var test = problemData.code.replace('__', answer);
     return eval(test);
   }
+
   function checkAnswerClick() {
     if (checkAnswer()) {
-      resultFlash.text('Correct!');
+      /* simple 
+      var correctFlash = learnjs.template('correct-flash');
+      correctFlash.find('a').attr('href', '#problem-' + (problemNumber + 1));
+      learnjs.flashElement(resultFlash, correctFlash);
+      */
+      var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, correctFlash);
     } else {
-      resultFlash.text('Incorrect!');
+      learnjs.flashElement(resultFlash, 'Incorrect!');
     }
     return false;
   }
+
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
   learnjs.applyObject(problemData, view);
   return view;
+}
+
+learnjs.buildCorrectFlash = function (problemNum) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNum < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNum + 1));
+  }
+  else {
+    link.attr('href', '');
+    link.text("You're Finished!");
+  }
+  return correctFlash;
+}
+
+learnjs.flashElement = function (elem, content) {
+  elem.fadeOut('fast', function () {
+    elem.html(content);
+    elem.fadeIn();
+  });
 }
 
 /* data */
@@ -69,3 +100,4 @@ learnjs.problems = [
   }
 ];
 //}
+
